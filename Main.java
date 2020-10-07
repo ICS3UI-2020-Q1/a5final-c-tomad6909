ba2dfda949d9e0d159a38c21fb2f916b208f5295
tomad6909 <tomad6909@wrdsb.ca>
2020-10-07 17:37:44 +0000

    Finished A5final

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will ask the user for a starting counting value and the value you want to stop at and then it will print out every number starting from the starting number all the way to the second number, going up by 2
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to input a number to start counting from
+    System.out.println("What number do you want to begin counting from?");
+    int startNumber = input.nextInt();
+
+    // ask the user what number to end the counting at
+    System.out.println("What number do you want to end counting at?");
+    int endNumber = input.nextInt();  
+
+    // create an if statement to determine if startnumber is even or odd
+    if(startNumber % 2 == 0){
+    } else{
+      startNumber++;
+    }
+
+    // create for loop to start counting
+    for(int count = startNumber; count <= endNumber; count = count + 2){
+      System.out.println(count);
+    }
     
   }
 }
